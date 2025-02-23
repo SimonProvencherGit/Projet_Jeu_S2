e18dee978dc418adc4409a0a25446521ba43fd09
amran <amrani2400@outlook.com>
2025-02-23 16:51:52 -0500

    .

---
+++ S2/JeuAvion/JeuAvion/SFX/include/SFX.h
@@ -0,0 +1,26 @@
+#ifndef SFX_H
+#define SFX_H
+
+#include <windows.h>
+#include <mmsystem.h>
+#include <mmreg.h>
+#include <thread>
+#include <iostream>
+#include <vector>
+#include <mutex>
+
+class SFX {
+public:
+    SFX();
+    ~SFX();
+    void PlaySFXAsync(LPCSTR lpszSound);
+
+private:
+    std::vector<HWAVEOUT> waveOutPool;
+    std::mutex poolMutex;
+    HWAVEOUT getAvailableWaveOut();
+    void releaseWaveOut(HWAVEOUT hWaveOut);
+};
+
+#endif
+#pragma once
